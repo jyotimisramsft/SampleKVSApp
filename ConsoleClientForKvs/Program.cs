f8fe3d0fbe008108b05c6c7a21f762b62c930be5
jyotimisramsft <jyotimisra@microsoft.com>
2022-06-22 18:04:45 +0530

    Create KVS app and console client

---
+++ ConsoleClientForKvs/Program.cs
@@ -0,0 +1,173 @@
+﻿using KvsActor.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using Microsoft.ServiceFabric.Actors.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors.KVSToRCMigration.Models;
+using System.IO;
+using System.Text;
+using KeyValuePair = Microsoft.ServiceFabric.Actors.KVSToRCMigration.Models.KeyValuePair;
+using System.Xml;
+using System.Runtime.Serialization;
+using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.FabricTransport;
+
+[assembly: FabricTransportServiceRemotingProvider(RemotingListenerVersion = RemotingListenerVersion.V2_1, RemotingClientVersion = RemotingClientVersion.V2_1)]
+namespace ConsoleClientForKvs
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+
+                //var res = GetAllAsync().GetAwaiter().GetResult(); 
+                /*string url = "http://MININT-U9I0MVN.fareast.corp.microsoft.com:30007/8df902ed-3bba-41f8-a43a-a612c8bf3a17/132929173713133919/50204944-66fa-4c3f-886b-a815bae6872f/";
+                var httpClient = new HttpClient()
+                {
+                BaseAddress = new Uri(url),
+                };
+                int count = 0;
+                var response = httpClient.SendAsync(CreateKvsApiRequestMessage(new Uri(url), 0, 1000), HttpCompletionOption.ResponseHeadersRead).GetAwaiter().GetResult();
+
+                var keyvaluepairserializer = new DataContractSerializer(typeof(List<KeyValuePair>));
+                //response.EnsureSuccessStatusCode();
+                using (var stream = response.Content.ReadAsStreamAsync().GetAwaiter().GetResult())
+                {
+                    using (var streamReader = new StreamReader(stream))
+                    {
+                        string responseLine = streamReader.ReadLine();
+                        while (responseLine != null)
+                        {
+
+                            List<KeyValuePair> kvsData = new List<KeyValuePair>();
+                            using (Stream memoryStream = new MemoryStream())
+                            {
+                                byte[] data = Encoding.UTF8.GetBytes(responseLine);
+                                memoryStream.Write(data, 0, data.Length);
+                                memoryStream.Position = 0;
+
+                                using (var reader = XmlDictionaryReader.CreateTextReader(memoryStream, XmlDictionaryReaderQuotas.Max))
+                                {
+                                    kvsData = (List<KeyValuePair>)keyvaluepairserializer.ReadObject(reader);
+                                    count += kvsData.Count;
+                                }
+                            }
+
+                            responseLine = streamReader.ReadLine();
+                        }
+                        Console.WriteLine("count = " + count );
+                    }
+                }*/
+
+
+                var myActor = GetActor("a000");
+                myActor.SetCountAsync(5, new CancellationToken()).GetAwaiter().GetResult();
+
+                myActor = GetActor("truce");
+                myActor.SetCountAsync(5, new CancellationToken()).GetAwaiter().GetResult();
+
+                myActor = GetActor("9ff");
+                myActor.SetCountAsync(5, new CancellationToken()).GetAwaiter().GetResult();
+                myActor.SetNameAsync("name", new CancellationToken()).GetAwaiter().GetResult();
+                myActor.SetByteArrayAsync(new byte[1], new CancellationToken()).GetAwaiter().GetResult();
+
+                myActor = GetActor("Orderlord");
+                myActor.SetCountAsync(5, new CancellationToken()).GetAwaiter().GetResult();
+                var task = Task.Run(async () => await myActor.GetCountAsync(new CancellationToken())).ConfigureAwait(false);
+                var result = task.GetAwaiter().GetResult();
+
+                Thread.Sleep(10000);
+                var task2 = Task.Run(async () => await myActor.GetCountAsync(new CancellationToken())).ConfigureAwait(false);
+                var result2 = task2.GetAwaiter().GetResult();
+
+                BulkCreateKvsActorAndAddStates(50);
+
+                var res = GetAllAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static EnumerationRequest CreateEnumerationRequestObject(long startSN, long enumerationSize)
+        {
+            var req = new EnumerationRequest();
+            req.StartSN = startSN;
+            req.ChunkSize = 100;
+            req.NoOfItems = 1000;
+            req.IncludeDeletes = false;
+
+            return req;
+        }
+
+        private static HttpRequestMessage CreateKvsApiRequestMessage(Uri baseUri, long startSN, long enumerationSize)
+        {
+            var requestserializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(typeof(EnumerationRequest));
+            var enumerationRequestContent = CreateEnumerationRequestObject(startSN, enumerationSize);
+
+            var stream = new MemoryStream();
+            requestserializer.WriteObject(stream, enumerationRequestContent);
+
+            var content = Encoding.UTF8.GetString(stream.ToArray());
+
+            return new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(baseUri, $"KvsMigration/EnumerateBySequenceNumber"),
+                Content = new StringContent(content, Encoding.UTF8, "application/json"),
+            };
+        }
+
+        private static async Task<string> GetAllAsync()
+        {
+            ActorId partitionKey = new ActorId("1507609b-69f5-452c-bd3d-cc2f9850e058");
+            var actorServiceProxy = ActorServiceProxy.Create(new Uri("fabric:/KvsAppForMigration/KvsActorService"), partitionKey);
+
+            ContinuationToken continuationToken = null;
+            CancellationToken cancellationToken = new CancellationToken(false);
+            List<ActorInformation> activeActors = new List<ActorInformation>();
+            string allActorId = "";
+            string allMarker = "";
+
+            do
+            {
+                PagedResult<ActorInformation> page = await actorServiceProxy.GetActorsAsync(continuationToken, cancellationToken);
+                activeActors.AddRange(page.Items);
+                continuationToken = page.ContinuationToken;
+                allMarker += continuationToken == null ? "" : continuationToken.Marker;
+            }
+            while (continuationToken != null);
+
+            for (int i = 0; i < activeActors.Count; i++)
+            {
+                allActorId += activeActors[i].ActorId.ToString() + "\n";
+            }
+            return activeActors.Count().ToString() + allMarker + "\n\n\n\n" + allActorId;
+        }
+
+        private static IKvsActor GetActor(string userId)
+        {
+            return ActorProxy.Create<IKvsActor>(
+                new ActorId(userId),
+                new Uri("fabric:/KvsAppForMigration/KvsActorService"));
+        }
+
+        private static void BulkCreateKvsActorAndAddStates(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var myActor = GetActor("user" + i.ToString());
+                myActor.SetCountAsync(5, new CancellationToken()).GetAwaiter().GetResult();
+            }
+            Console.WriteLine("Created " + count + " actors");
+        }
+    }
+}
---
+++ KvsActor/KvsActor.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Runtime;
+using KvsActor.Interfaces;
+using Microsoft.ServiceFabric.Actors.Runtime.Migration;
+
+namespace KvsActor
+{
+    /// <remarks>
+    /// This class represents an actor.
+    /// Every ActorID maps to an instance of this class.
+    /// The StatePersistence attribute determines persistence and replication of actor state:
+    ///  - Persisted: State is written to disk and replicated.
+    ///  - Volatile: State is kept in memory only and replicated.
+    ///  - None: State is kept in memory only and not replicated.
+    /// </remarks>
+
+    [StateMigration(StateMigration.Source)]
+    [StatePersistence(StatePersistence.Persisted)]
+    //internal class KvsActor : Actor, IKvsActor, IRemindable
+    internal class KvsActor : Actor, IKvsActor
+    {
+        /// <summary>
+        /// Initializes a new instance of KvsActor
+        /// </summary>
+        /// <param name="actorService">The Microsoft.ServiceFabric.Actors.Runtime.ActorService that will host this actor instance.</param>
+        /// <param name="actorId">The Microsoft.ServiceFabric.Actors.ActorId for this actor instance.</param>
+        public KvsActor(ActorService actorService, ActorId actorId) 
+            : base(actorService, actorId)
+        {
+        }
+
+        /// <summary>
+        /// This method is called whenever an actor is activated.
+        /// An actor is activated the first time any of its methods are invoked.
+        /// </summary>
+        protected override async Task OnActivateAsync()
+        {
+            ActorEventSource.Current.ActorMessage(this, "Actor activated.");
+
+            // The StateManager is this actor's private state store.
+            // Data stored in the StateManager will be replicated for high-availability for actors that use volatile or persisted state storage.
+            // Any serializable object can be saved in the StateManager.
+            // For more information, see https://aka.ms/servicefabricactorsstateserialization
+
+            /*string reminderName = "Increment count";
+
+            IActorReminder reminderRegistration = await this.RegisterReminderAsync(
+                reminderName,
+                BitConverter.GetBytes(5),
+                TimeSpan.FromSeconds(0),    //The amount of time to delay before firing the reminder
+                TimeSpan.FromSeconds(5));    //The time interval between firing of reminders
+
+            await this.StateManager.TryAddStateAsync("count", 0);*/
+
+        }
+
+        /*public async Task ReceiveReminderAsync(string reminderName, byte[] context, TimeSpan dueTime, TimeSpan period)
+        {
+            if (reminderName.Equals("Increment count"))
+            {
+                var count = await this.StateManager.GetStateAsync<int>("count", new CancellationToken());
+                count += 5;
+                await this.StateManager.AddOrUpdateStateAsync("count", count, (key, value) => count > value ? count : value, new CancellationToken());
+            }
+        }*/
+
+        /// <summary>
+        /// TODO: Replace with your own actor method.
+        /// </summary>
+        /// <returns></returns>
+        Task<int> IKvsActor.GetCountAsync(CancellationToken cancellationToken)
+        {
+            return this.StateManager.GetStateAsync<int>("count", cancellationToken);
+        }
+
+        /// <summary>
+        /// TODO: Replace with your own actor method.
+        /// </summary>
+        /// <returns></returns>
+        Task<string> IKvsActor.GetNameAsync(CancellationToken cancellationToken)
+        {
+            return this.StateManager.GetStateAsync<string>("name", cancellationToken);
+        }
+
+        /// <summary>
+        /// TODO: Replace with your own actor method.
+        /// </summary>
+        /// <returns></returns>
+        Task<byte[]> IKvsActor.GetByteArrayAsync(CancellationToken cancellationToken)
+        {
+            return this.StateManager.GetStateAsync<byte[]>("array", cancellationToken);
+        }
+
+        /// <summary>
+        /// TODO: Replace with your own actor method.
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        Task IKvsActor.SetCountAsync(int count, CancellationToken cancellationToken)
+        {
+            // Requests are not guaranteed to be processed in order nor at most once.
+            // The update function here verifies that the incoming count is greater than the current count to preserve order.
+            return this.StateManager.AddOrUpdateStateAsync("count", count, (key, value) => count > value ? count : value, cancellationToken);
+        }
+
+        Task IKvsActor.SetNameAsync(string name, CancellationToken cancellationToken)
+        {
+            // Requests are not guaranteed to be processed in order nor at most once.
+            // The update function here verifies that the incoming count is greater than the current count to preserve order.
+            return this.StateManager.AddOrUpdateStateAsync("name", name, (key, value) => name, cancellationToken);
+        }
+
+        Task IKvsActor.SetByteArrayAsync(byte[] byteArray, CancellationToken cancellationToken)
+        {
+            // Requests are not guaranteed to be processed in order nor at most once.
+            // The update function here verifies that the incoming count is greater than the current count to preserve order.
+            return this.StateManager.AddOrUpdateStateAsync("array", byteArray, (key, value) => byteArray, cancellationToken);
+        }
+    }
+}
